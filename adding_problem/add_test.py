187b599239338223e254af035d82010d27d71287
YuanTingHsieh <yhsieh28@wisc.edu>
2018-07-11 13:17:04 -0700

    update default params

--- adding_problem/add_test.py
+++ adding_problem/add_test.py
@@ -20,20 +20,20 @@ parser.add_argument('--clip', type=float, default=-1,
                     help='gradient clip, -1 means no clip (default: -1)')
 parser.add_argument('--epochs', type=int, default=10,
                     help='upper epoch limit (default: 10)')
-parser.add_argument('--ksize', type=int, default=7,
-                    help='kernel size (default: 7)')
-parser.add_argument('--levels', type=int, default=8,
-                    help='# of levels (default: 8)')
-parser.add_argument('--seq_len', type=int, default=400,
-                    help='sequence length (default: 400)')
+parser.add_argument('--ksize', type=int, default=6,
+                    help='kernel size (default: 6)')
+parser.add_argument('--levels', type=int, default=7,
+                    help='# of levels (default: 7)')
+parser.add_argument('--seq_len', type=int, default=200,
+                    help='sequence length (default: 200)')
 parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='report interval (default: 100')
 parser.add_argument('--lr', type=float, default=4e-3,
                     help='initial learning rate (default: 4e-3)')
 parser.add_argument('--optim', type=str, default='Adam',
                     help='optimizer to use (default: Adam)')
-parser.add_argument('--nhid', type=int, default=30,
-                    help='number of hidden units per layer (default: 30)')
+parser.add_argument('--nhid', type=int, default=27,
+                    help='number of hidden units per layer (default: 27)')
 parser.add_argument('--seed', type=int, default=1111,
                     help='random seed (default: 1111)')
 args = parser.parse_args()
